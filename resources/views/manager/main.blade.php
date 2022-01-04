68a43f3a3cebbabe5efcb85e15ff0e313815c551
milan <khimdingmilan99@gmail.com>
2022-01-04 13:54:55 +0545

    logo is made dynamic

--- resources/views/manager/main.blade.php
+++ resources/views/manager/main.blade.php
@@ -78,7 +78,9 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
       <a href="#" class="brand-link">
-        <img src="{{ asset('image/IMG.jpg') }}" alt="Techware Logo" class="brand-image elevation-3">
+
+        <img :src="`/image/manager/thumbnail/{{Auth::user()->image_enc}}`" alt="Techware Logo" class="brand-image elevation-3">
+        {{-- <img src="{{ asset('image/IMG.jpg') }}" alt="Techware Logo" class="brand-image elevation-3"> --}}
         <span class="brand-text font-weight-light">Manager</span>
       </a>
 
